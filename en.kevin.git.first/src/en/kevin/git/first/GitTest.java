041808e3dd050c51e49a66216e28b1900e343a3b
Kevin Sivabalan <jbosshellokitty@gmail.com>
2014-09-14 12:39:07 -0400

    Initial Commit via Ecplise IDE

---
+++ en.kevin.git.first/src/en/kevin/git/first/GitTest.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package en.kevin.git.first;
+
+/**
+ * @author Cloud
+ *
+ */
+public class GitTest {
+	public static void main(String[] args){
+		System.out.println("Git is Fun!!!!");
+	}
+}
